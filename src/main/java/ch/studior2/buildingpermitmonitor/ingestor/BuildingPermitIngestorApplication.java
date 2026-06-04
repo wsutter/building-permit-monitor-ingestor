e77824cb36c68ab519e3ddaec79815c0eed82c3f
walter <walter.sutter@meticulous.ch>
2026-06-04 17:35:49 +0200

    NO-ISSUE Additional DLQs

--- src/main/java/ch/studior2/buildingpermitmonitor/ingestor/BuildingPermitIngestorApplication.java
+++ src/main/java/ch/studior2/buildingpermitmonitor/ingestor/BuildingPermitIngestorApplication.java
@@ -1,12 +1,15 @@
 package ch.studior2.buildingpermitmonitor.ingestor;
 
+import ch.studior2.buildingpermitmonitor.contracts.config.KafkaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@Import(KafkaConfig.class)
 @EnableJpaRepositories(basePackages = "ch.studior2.buildingpermitmonitor.persistence.repository")
 @EntityScan(basePackages = "ch.studior2.buildingpermitmonitor.persistence.entity")
 @SpringBootApplication
